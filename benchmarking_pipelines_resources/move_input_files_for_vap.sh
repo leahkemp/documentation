804677cb36985f7ae7391dcc6b2d31b6b208956f
Leah Kemp <leah.kemp@esr.cri.nz>
2020-09-25 12:08:14 +1200

    Fix code

--- benchmarking_pipelines_resources/move_input_files_for_vap.sh
+++ benchmarking_pipelines_resources/move_input_files_for_vap.sh
@@ -6,6 +6,7 @@ for vcfdir in exome/*/*/*/*/vcf; do
 done
 
 # Move bam files output human_genomics_pipeline
-for bamdir in exome/*/*/*/*/bam; do
-    cp $bamdir/../human_genomics_pipeline/results/mapped/* $bamdir
+for bamdir in exome/*/*/*/*/bams; do
+    cp $bamdir/../human_genomics_pipeline/results/mapped/*_recalibrated.bai $bamdir
+    cp $bamdir/../human_genomics_pipeline/results/mapped/*_recalibrated.bam $bamdir
 done
